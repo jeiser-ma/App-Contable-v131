6df413e45fe56eae2f07c229678f8b539cbecda1
Jeiser Medrano Abreu <jeiser.medrano@factory.generalsoftwareinc.com>
2026-07-25 00:35:53 -0300

    Update app version to 1.3.5 and last update date to 2026-07-24 in configs.js

--- js/configs.js
+++ js/configs.js
@@ -4,8 +4,8 @@
 //#region Constants
 
 /** Versión de la app. Único lugar a actualizar al publicar cambios (la PWA se actualiza al cambiar esto). */
-const APP_VERSION = "1.3.4";
-const APP_LAST_UPDATE = "2026-07-23";
+const APP_VERSION = "1.3.5";
+const APP_LAST_UPDATE = "2026-07-24";
 
 /**
  * Devuelve la versión de la app (por compatibilidad con código que usa getAppVersion().then(...)).
