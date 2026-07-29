20a95431581cf0d5f8c9a953a38ec2cc96e8a564
Jeiser Medrano Abreu <jeiser.medrano@factory.generalsoftwareinc.com>
2026-07-28 22:41:18 -0300

    Increment app version to 1.3.10 in configs.js following recent updates.

--- js/configs.js
+++ js/configs.js
@@ -4,7 +4,7 @@
 //#region Constants
 
 /** Versión de la app. Único lugar a actualizar al publicar cambios (la PWA se actualiza al cambiar esto). */
-const APP_VERSION = "1.3.9";
+const APP_VERSION = "1.3.10";
 const APP_LAST_UPDATE = "2026-07-28";
 
 /**
